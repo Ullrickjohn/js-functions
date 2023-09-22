162d288e7d60ff2a5e1e9b3180e0a7ffd0d198d2
Ullrickjohn <example@gnail.com>
2023-09-22 22:20:40 +0500

    first commit complated change color on click in js

---
+++ script.js
@@ -0,0 +1,19 @@
+// const title = document.getElementsByTagName('h1')
+
+// console.log(title);
+// const title = document.querySelector('h1')
+// console.log(title);
+// const title = document.querySelectorAll('h1')
+// console.log(document);
+// title.textContent = 'magap'
+// title.style.color = 'red'
+// title.style.background = 'blue'
+const block = document.querySelector('.block')
+
+// block.onclick = function(){
+// block.style.background = 'red'
+// }
+block.addEventListener('click', function(){
+block.style.background = 'blue'
+})
+
